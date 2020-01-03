c644e9ee814b05e906de91c9f33ecf809f615a69
albertas-inventi <albertas@inventi.lt>
2020-01-03 13:39:51 +0200

    Remove rotation time as its not supported for ASYMMETRIC_SIGN keys

--- main.tf
+++ main.tf
@@ -34,7 +34,6 @@ resource "google_kms_crypto_key" "key" {
   count           = var.prevent_destroy ? length(var.keys) : 0
   name            = var.keys[count.index]
   key_ring        = google_kms_key_ring.key_ring.self_link
-  rotation_period = var.key_rotation_period
   purpose         = var.purpose
  
   version_template {
